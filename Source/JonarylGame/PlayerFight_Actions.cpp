ea82218ba8536ae7abfb2265969e2c57f7fe0baf
Cellura Julian <julian.cellura@gmail.com>
2023-03-07 17:00:07 +0100

    saut debug mais c pas encore ça

--- Source/JonarylGame/PlayerFight_Actions.cpp
+++ Source/JonarylGame/PlayerFight_Actions.cpp
@@ -22,34 +22,149 @@ void APlayerFight_Actions::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
 
+    if (CurrentState == APlayerFight_States::EPlayerFight_State::Jump || CurrentState == APlayerFight_States::EPlayerFight_State::DashJump)
+    {
+        int sens = 1;
+        if (CurrentState == APlayerFight_States::EPlayerFight_State::DashJump)
+        {
+            sens = -1;
+            if (forwardSpeed != 0)
+                forwardSpeed = forwardSpeed * 20;
+        }
+
+        const FVector upvector = GetActorUpVector() * sens;
+        FVector forwardVector;
+        if (forwardSpeed != 0)
+        {
+            forwardVector = GetActorForwardVector();
+        }
+        else
+        {
+            forwardVector = FVector(0,0,0);
+        }
+        const FVector relativeMoveVector = FVector(XMoveDirection*20, -YMoveDirection*20, 0);
+        FVector worldMoveVector = GetActorRotation().RotateVector(relativeMoveVector);
+
+        /////////////////// raycast ///////////////////////////////////////////////////////// 
+        const FVector force = (upvector * jumpSpeed) + (forwardVector * forwardSpeed) + (worldMoveVector * 1000);
+
+        float GroundCheckDistance = 1.0f;
+        // Raycast pour détecter si le personnage est en collision avec le sol
+        FVector StartLocation = GetActorLocation();
+        FVector EndLocation = StartLocation - FVector(0.0f, 0.0f, GroundCheckDistance);
+        FHitResult HitResult;
+        FCollisionQueryParams QueryParams;
+        QueryParams.AddIgnoredActor(this);
+
+        bool bIsGrounded = GetWorld()->LineTraceSingleByChannel(HitResult, StartLocation, EndLocation, ECC_Visibility, QueryParams);
+
+        if (bIsGrounded)
+        {
+            float DistanceToGround = HitResult.Distance;
+            UE_LOG(LogTemp, Warning, TEXT("raycast On %f"), DistanceToGround);
+            SetCharacterState(APlayerFight_States::EPlayerFight_State::Idle);
+        }
+        /////////////////// raycast ///////////////////////////////////////////////////////// 
+
+        PlayerController->GetCharacter()->GetCharacterMovement()->AddForce(force);
+
+        // Contrôler la vitesse du saut
+        if (jumpSpeed > 2000000.0f)
+        {
+            jumpSpeed = 50000.0f;
+            if (forwardSpeed != 0)
+            {
+                forwardSpeed = 90000.0f;
+            }
+        }
+        else if (jumpSpeed > 0.0f) {
+            jumpSpeed -= 50000.0f * GetWorld()->GetDeltaSeconds();
+            if (forwardSpeed != 0)
+            {
+                forwardSpeed -= 50000.0f * GetWorld()->GetDeltaSeconds();
+            }
+        }
+        else {
+            jumpSpeed = 0.0f;
+            SetCharacterState(APlayerFight_States::EPlayerFight_State::IdleJump);
+        }
+    }
+    else if (CurrentState == APlayerFight_States::EPlayerFight_State::DashJump)
+    {
+
+    }
 }
 
 void APlayerFight_Actions::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-    PlayerInputComponent->BindAction("ABtn", IE_Pressed, this, &APlayerFight_Actions::AButton);
+    if (EnhancedInputComponent)
+    {
+        //EnhancedInputComponent->BindAction(ABtn, ETriggerEvent::Triggered, this, &APlayerFight_Actions::ABtnAction); // space
+        //EnhancedInputComponent->BindAction(BBtn, ETriggerEvent::Triggered, this, &APlayerFight_Actions::BButton); // A
+        //EnhancedInputComponent->BindAction(XBtn, ETriggerEvent::Triggered, this, &APlayerFight_Actions::XButton); // E
+        //EnhancedInputComponent->BindAction(YBtn, ETriggerEvent::Triggered, this, &APlayerFight_Actions::YButton); // R
 
-    PlayerInputComponent->BindAction("Bbtn", IE_Pressed, this, &APlayerFight_Actions::BButton);
-
-    PlayerInputComponent->BindAction("Xbtn", IE_Pressed, this, &APlayerFight_Actions::XButton);
-
-    PlayerInputComponent->BindAction("Ybtn", IE_Pressed, this, &APlayerFight_Actions::YButton);
+    }
 }
 
-void APlayerFight_Actions::AButton()
+
+void APlayerFight_Actions::ABtnAction()
 {
-    UE_LOG(LogTemp, Warning, TEXT("A button pressed with value %f"), "value");
-}
-void APlayerFight_Actions::BButton()
-{
-    UE_LOG(LogTemp, Warning, TEXT("B button pressed with value %f"), "value");
-}
-void APlayerFight_Actions::XButton()
-{
-    UE_LOG(LogTemp, Warning, TEXT("X button pressed with value %f"), "value");
-}
-void APlayerFight_Actions::YButton()
-{
-    UE_LOG(LogTemp, Warning, TEXT("Y button pressed with value %f"), "value");
+    UE_LOG(LogTemp, Warning, TEXT(" CurrentState action = %d"), CurrentState);
+
+
+    if (CurrentState != APlayerFight_States::EPlayerFight_State::Jump && CurrentState != APlayerFight_States::EPlayerFight_State::IdleJump && 
+        CurrentState != APlayerFight_States::EPlayerFight_State::DashJump)
+    {
+        UE_LOG(LogTemp, Warning, TEXT(" Jump action = %d"), CurrentState);
+        jumpSpeed = 2400000.0f;
+        if (FMath::Abs(XMoveDirection) + FMath::Abs(YMoveDirection) > 0.0f)
+        {
+            forwardSpeed = 180000.0f;
+        }
+        else
+        {
+            forwardSpeed = 0.0f;
+        }
+
+        if (PlayerController->GetCharacter() && PlayerController->GetCharacter()->GetCharacterMovement()) 
+        {
+            UE_LOG(LogTemp, Warning, TEXT(" Jump PlayerController = %d"), CurrentState);
+            XMoveDirection = 0.0f;
+            YMoveDirection = 0.0f;
+            UE_LOG(LogTemp, Error, TEXT("Jump"));
+            SetCharacterState(APlayerFight_States::EPlayerFight_State::Jump);
+        }
+        else {
+            UE_LOG(LogTemp, Error, TEXT("PlayerMesh is null"));
+        }
+    }
+    else if (CurrentState == APlayerFight_States::EPlayerFight_State::IdleJump)
+    {
+        UE_LOG(LogTemp, Warning, TEXT(" CurrentState IdleJump = %d"), CurrentState);
+        jumpSpeed = 2400000.0f;
+        if (FMath::Abs(XMoveDirection) + FMath::Abs(YMoveDirection) > 0.0f)
+        {
+            forwardSpeed = 180000.0f;
+        }
+        else
+        {
+            forwardSpeed = 0.0f;
+        }
+
+        if (PlayerController->GetCharacter() && PlayerController->GetCharacter()->GetCharacterMovement())
+        {
+            UE_LOG(LogTemp, Warning, TEXT(" CurrentState PlayerController = %d"), CurrentState);
+
+            XMoveDirection = 0.0f;
+            YMoveDirection = 0.0f;
+            UE_LOG(LogTemp, Error, TEXT("DashJump"));
+            SetCharacterState(APlayerFight_States::EPlayerFight_State::DashJump);
+        }
+        else {
+            UE_LOG(LogTemp, Error, TEXT("PlayerMesh is null"));
+        }
+    }
 }
--- Source/JonarylGame/PlayerFight_Move.h
+++ Source/JonarylGame/PlayerFight_Move.h
@@ -22,6 +22,8 @@
 
 #include "inputActionValue.h"
 
+#include "PlayerFight_States.h"
+
 #include "PlayerFight_Move.generated.h"
 
 
@@ -40,6 +42,8 @@ public:
 	// Sets default values for this character's properties
 	APlayerFight_Move();
 
+	APlayerFight_States::EPlayerFight_State CurrentState = APlayerFight_States::EPlayerFight_State::Idle;
+
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 		USpringArmComponent* CameraBoom;
 
@@ -50,19 +54,25 @@ public:
 		bool bEnableInterpolation = true;
 
 	UPROPERTY(EditAnywhere, Category = "Movement")
-		int runSpeed = 5 + Classe_SpeedMove;
+		float maxRunSpeed = Classe_SpeedMove;
+	int runSpeed = 20;
 	UPROPERTY(EditAnywhere, Category = "Movement")
 		int runRotationSpeed = 5 + Classe_SpeedRotate;
 	
 	float valueSpeed = 0.0f;
 	float valueSpeedJoy = 0.0f;
+	float InitialSpeed;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void Landed(const FHitResult& Hit) override;
 
 	//void MoveForward(float Value);
 	void MoveForward(const FInputActionValue& Value);
+	void dddddMoveForward(const FInputActionValue& Value);
 	void TurnCamera(const FInputActionValue& Value);
+	void StartMoving();
+	void StopMoving();
 
 	void MoveRight(float Value);
 
@@ -70,6 +80,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
+	virtual void SetCharacterState(APlayerFight_States::EPlayerFight_State NewState);
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 		UInputMappingContext* PlayerMappingContext;
@@ -78,12 +89,49 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
 		UInputAction* MoveCamera;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* ABtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* BBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* XBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* YBtn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* RBBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* LBBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* RTBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* LTBtn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* TopBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* BottomBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* LeftBtn;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+		UInputAction* RightBtn;
+
+
 	APlayerController* PlayerController;
 	UEnhancedInputComponent* EnhancedInputComponent;
 	bool isMoveInput;
 
+	float XMoveDirection;
+	float YMoveDirection;
+
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "TEST")
 		UStaticMeshComponent* MyMeshTest;
+
+
+	virtual void ABtnAction();
+	//virtual void BButton();
+	//virtual void XButton();
+	//virtual void YButton();
 	
 };
